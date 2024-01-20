e3624060980efc0feedde06bb5466acef8f28ed0
Zjuntong <127921522+Zjuntong@users.noreply.github.com>
2024-01-20 12:27:00 +0800

    Update weather_report.py

--- weather_report.py
+++ weather_report.py
@@ -1,16 +1,19 @@
-# 安装依赖 pip3 install requests html5lib bs4 schedule
-import os
+import time
 import requests
 import json
+import schedule
 from bs4 import BeautifulSoup
 
+
 # 从测试号信息获取
-appID = os.environ.get("wxaf824accdbfdad5a")
-appSecret = os.environ.get("732ca5c31a4363b0d344013f7b7dd3e7")
-# 收信人ID即 用户列表中的微信号
-openId = os.environ.get("oKaJ26nC7rPHTReZSbXo4cv6OX9E")
-# 天气预报模板ID
-weather_template_id = os.environ.get("bzHc-S8oyAQvMl6MopOiEfsYgs2boMxSokMr7W_BpfE")
+appID = "wxaf824accdbfdad5a"
+appSecret = "732ca5c31a4363b0d344013f7b7dd3e7"
+#收信人ID即 用户列表中的微信号，见上文
+openId = "oKaJ26nC7rPHTReZSbXo4cv6OX9E"
+# 天气预报模板ID 
+weather_template_id = "bzHc-S8oyAQvMl6MopOiEfsYgs2boMxSokMr7W_BpfE"
+
+
 
 def get_weather(my_city):
     urls = ["http://www.weather.com.cn/textFC/hb.shtml",
@@ -117,17 +120,44 @@ def send_weather(access_token, weather):
     print(requests.post(url, json.dumps(body)).text)
 
 
+def send_timetable(access_token, message):
+    body = {
+        "touser": openId,
+        "template_id": timetable_template_id.strip(),
+        "url": "https://weixin.qq.com",
+        "data": {
+            "message": {
+                "value": message
+            },
+        }
+    }
+    url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}'.format(access_token)
+    print(requests.post(url, json.dumps(body)).text)
 
-def weather_report(this_city):
+
+def weather_report(city):
     # 1.获取access_token
     access_token = get_access_token()
     # 2. 获取天气
-    weather = get_weather(this_city)
+    weather = get_weather(city)
     print(f"天气信息： {weather}")
     # 3. 发送消息
     send_weather(access_token, weather)
 
 
+def timetable(message):
+    # 1.获取access_token
+    access_token = get_access_token()
+    # 3. 发送消息
+    send_timetable(access_token, message)
+
 
 if __name__ == '__main__':
     weather_report("宁波")
+    # timetable("第二教学楼十分钟后开始英语课")
+
+    # schedule.every().day.at("18:30").do(weather_report, "南京")
+    # schedule.every().monday.at("13:50").do(timetable, "第二教学楼十分钟后开始英语课")
+    #while True:
+    #    schedule.run_pending()
+    #    time.sleep(1)
